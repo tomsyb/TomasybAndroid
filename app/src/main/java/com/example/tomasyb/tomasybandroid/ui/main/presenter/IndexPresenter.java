c9e00af1134becfe446edff19174699c765683d6
yanbo <760375443@qq.com>
2018-06-05 18:54:09 +0800

    mvp层建立p层

---
+++ app/src/main/java/com/example/tomasyb/tomasybandroid/ui/main/presenter/IndexPresenter.java
@@ -0,0 +1,13 @@
+package com.example.tomasyb.tomasybandroid.ui.main.presenter;
+
+/**
+ * 首页Presenter
+ *
+ * @author 严博
+ * @version 1.0.0
+ * @date 2018-6-5.11:16
+ * @since JDK 1.8
+ */
+
+public class IndexPresenter {
+}
