5722cb60b974bd631fa4179bc1e57e58f8ddd286
yanbo <760375443@qq.com>
2018-07-09 09:32:47 +0800

    学习主页面搭建

--- app/src/main/java/com/example/tomasyb/tomasybandroid/ui/study/StudyMainFragment.java
+++ app/src/main/java/com/example/tomasyb/tomasybandroid/ui/study/StudyMainFragment.java
@@ -1,9 +1,18 @@
 package com.example.tomasyb.tomasybandroid.ui.study;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.tomasyb.baselib.base.BaseFragment;
 import com.example.tomasyb.tomasybandroid.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * 学习主Fragment
@@ -14,17 +23,22 @@ import com.example.tomasyb.tomasybandroid.R;
  * @since JDK 1.8
  */
 
-public class StudyMainFragment extends BaseFragment{
+public class StudyMainFragment extends BaseFragment {
     private static final String ARG_TITLE = "title";
+    @BindView(R.id.rv)
+    RecyclerView mRv;
+
+
     /**
      * 单列获取fragment
+     *
      * @param title
      * @return
      */
-    public static StudyMainFragment getInstance(String title){
+    public static StudyMainFragment getInstance(String title) {
         StudyMainFragment fragment = new StudyMainFragment();
         Bundle bundle = new Bundle();
-        bundle.putString(ARG_TITLE,title);
+        bundle.putString(ARG_TITLE, title);
         fragment.setArguments(bundle);
         return fragment;
     }
@@ -41,6 +55,14 @@ public class StudyMainFragment extends BaseFragment{
 
     @Override
     protected void initView() {
+        initData();
+        mRv.setLayoutManager(new LinearLayoutManager(mRv.getContext()));
+        mRv.setAdapter();
+    }
+
+    private void initData() {
 
     }
+
+
 }
