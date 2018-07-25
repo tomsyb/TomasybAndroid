161d9ea08acebf46ed6891bed0ec65d5b4449998
yanbo <760375443@qq.com>
2018-07-25 18:15:19 +0800

    添加下拉刷新功能

---
+++ baselib/src/main/java/com/example/tomasyb/baselib/refresh/listener/CoordinatorLayoutListener.java
@@ -0,0 +1,5 @@
+package com.example.tomasyb.baselib.refresh.listener;
+
+public interface CoordinatorLayoutListener {
+    void onCoordinatorUpdate(boolean enableRefresh, boolean enableLoadMore);
+}
