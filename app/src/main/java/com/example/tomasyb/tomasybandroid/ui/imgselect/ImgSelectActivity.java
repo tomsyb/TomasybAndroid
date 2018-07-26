bb00f6842645f60c90e4a36a3d24c0a8462720d8
yanbo <760375443@qq.com>
2018-07-26 20:15:48 +0800

    添加下拉刷新功能

---
+++ app/src/main/java/com/example/tomasyb/tomasybandroid/ui/imgselect/ImgSelectActivity.java
@@ -0,0 +1,18 @@
+package com.example.tomasyb.tomasybandroid.ui.imgselect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tomasyb.tomasybandroid.R;
+
+/**
+ * 图片选择列子
+ */
+public class ImgSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_img_select);
+    }
+}
