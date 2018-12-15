9a0a344f6a061a1187d8155c72d91ba3a2cbeaee
yanbo <760375443@qq.com>
2018-12-15 19:35:56 +0800

    自定义view构造方法解析

--- app/src/main/java/com/example/tomasyb/tomasybandroid/ui/view/CustomNetDialog.java
+++ app/src/main/java/com/example/tomasyb/tomasybandroid/ui/view/CustomNetDialog.java
@@ -14,9 +14,10 @@ import android.view.View;
  * @since JDK 1.8
  */
 
-public class CustomNetDialog extends View{
+public class CustomNetDialog extends View {
     /**
      * 代码里new的时候调用
+     *
      * @param context
      */
     public CustomNetDialog(Context context) {
@@ -25,8 +26,9 @@ public class CustomNetDialog extends View{
 
     /**
      * xml布局中声明自动调用此方法
+     *
      * @param context
-     * @param attrs 自定义属性是从这传进来的
+     * @param attrs   自定义属性是从这传进来的
      */
     public CustomNetDialog(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -35,10 +37,10 @@ public class CustomNetDialog extends View{
     /**
      * 不会主动调用，一般在第二个构造函数里主动调用，如View有style属性时
      * 4个构造方法和这个3个构造方法一样4个构造方法在API21之后才使用
+     *
      * @param context
      * @param attrs
      * @param defStyleAttr
-     *
      */
     public CustomNetDialog(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
@@ -47,11 +49,30 @@ public class CustomNetDialog extends View{
 
     /**
      * 测量View的大小
+     * 参数其实不是宽高，而是由宽高各自方向上对应的测量模式来合成的一个值
+     * 模式有3种UNSPECIFIED EXACTLY  AT_MOST
+     * int widthmode = MeasureSpec.getMode(widthMeasureSpec);// 获取宽度的测量模式
+     * int widthsize = MeasureSpec.getSize(widthMeasureSpec);// 获取宽度的确切数值
+     * 高度同理
+     *
      * @param widthMeasureSpec
      * @param heightMeasureSpec
      */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    }
+
+    /**
+     * 在视图大小发送改变时调用
+     * @param w
+     * @param h
+     * @param oldw
+     * @param oldh
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
     }
 }
